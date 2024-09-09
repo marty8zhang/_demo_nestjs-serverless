dbb8439f1f30c1dcc305f7a35cc874672032879d
Marty Zhang <marty8zhang@gmail.com>
2024-09-09 21:43:16 +1000

    Give the Lambda function access to resources in the AWS VPC.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import * as process from 'node:process';
 
+import { HttpService } from '@nestjs/axios';
 import { Controller, Get } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -22,5 +27,14 @@ export class AppController {
       password: process.env.DB_PASSWORD,
       databaseName: process.env.DB_DBNAME,
     };
+  }
+
+  @Get('egghdz')
+  async getEgghdz() {
+    const url = `http://${process.env.EGGHDZ_V1_ADDRESS}`;
+    console.log(`Hitting ${url}...`);
+    const response = await firstValueFrom(this.httpService.get(url));
+
+    return response.data;
   }
 }
